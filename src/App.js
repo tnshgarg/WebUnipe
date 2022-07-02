120eb754f84d114141c74429fca304adffce44e0
tnshgarg <tanish@binaryveda.com>
2022-07-02 21:58:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import CSVReader from "react-csv-reader"
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const papaparseOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: (header) => header.toLowerCase().replace(/\W/g, "_"),
+  }
+
+  const [dataEntry, setDataEntry] = useState()
+
+  const handleForce = (data, fileInfo) => {
+    setDataEntry(data)
+    data.map((item) => {
+    try {
+      axios.post("http://ed4b-117-214-159-246.ngrok.io", {
+        name: item.name,
+            age: item.age,
+            gender: item.gender
+    })}catch (err) {
+      console.error(err)
+    }
+  })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CSVReader
+        cssClass="csv-reader-input"
+        label="Select CSV"
+        onFileLoaded={handleForce}
+        parserOptions={papaparseOptions}
+        inputId="ObiWan"
+        inputName="ObiWan"
+        inputStyle={{ color: "green" }}
+      />
+      {dataEntry ? (
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+          </tr>
+          {dataEntry.map((item) => (
+            <tr>
+              <th contentEditable={true}>{item.name}</th>
+              <th contentEditable={true}>{item.age}</th>
+              <th contentEditable={true}>{item.gender}</th>
+            </tr>
+          ))}
+        </table>
+      ) : (
+        <h1>Hi, Please upload a CSV File and see the magic.</h1>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgObFIgR29lCES9ZIVjaFfhbRB7CkKpU4",
+  authDomain: "webassignmentforunipe.firebaseapp.com",
+  projectId: "webassignmentforunipe",
+  storageBucket: "webassignmentforunipe.appspot.com",
+  messagingSenderId: "1055535367681",
+  appId: "1:1055535367681:web:04460d65bf109161cd2200"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
